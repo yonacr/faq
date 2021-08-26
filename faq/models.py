70efb3d313e2413760688493e4248331672254ba
yonacr <yona.rasolonjatovo@staff.blueline.mg>
2021-08-26 20:49:33 +0300

    add: database modeling

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(
+        max_length=256
+    )
+
+
+class Answer(models.Model):
+
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    creation_date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+
+class Question(models.Model):
+
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+    creation_date = models.DateTimeField(
+        auto_now_add=True
+    )
+    answer = models.ForeignKey(
+        Answer,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+# EOF
