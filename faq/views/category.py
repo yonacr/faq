98a70ca4b55834cf3872408f5db606701f313f1b
yonacr <yona.rasolonjatovo@staff.blueline.mg>
2021-08-28 11:00:52 +0300

    add: search functionnality

--- faq/views/category.py
+++ faq/views/category.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
+from django.views.generic.base import View
 
 from faq.models import Category, Question
 
@@ -15,9 +17,23 @@ class CategoryView(LoginRequiredMixin, TemplateView):
         return context
 
 
-class CategoryListView(LoginRequiredMixin, ListView):
+class CategoryListView(LoginRequiredMixin, View):
+    """
+    A view that displays all categories and a Question search bar
+    """
 
-    model = Category
     template_name = "category_list.html"
+
+    def get(self, request, **kwargs):
+        context = {
+            'categories': Category.objects.all(),
+            'kwargs': kwargs,
+            'self_kwargs': request.GET.get('q', '')
+        }
+        if request.GET.get('q'):
+            context['result'] = Question.objects.filter(
+                text__icontains=request.GET.get('q')
+            )
+        return render(request, self.template_name, context)
 
 # EOF
