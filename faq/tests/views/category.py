28fd7a6ad347ae483c6958fe9c120a1da2625d3d
yonacr <yona.rasolonjatovo@staff.blueline.mg>
2021-08-28 13:09:26 +0300

    fix: data already created from data migration

--- faq/tests/views/category.py
+++ faq/tests/views/category.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 from faq.views.category import CategoryView
+from faq.models import Category
 
 from model_bakery import baker
 
@@ -16,10 +17,10 @@ class TestCategoryView(TestCase):
         cls.client = Client()
 
     def test_get_context_data_with_existing_category_kwargs(self):
-        category = baker.make('Category')
+        category = Category.objects.first()
         self.client.force_login(self.user)
         response = self.client.get(
-            reverse('faq:category', kwargs={'id': 1}),
+            reverse('faq:category', kwargs={'id': category.id}),
         )
         self.assertIn('category', response.context)
         self.assertEqual(response.context['category'], category)
