7d429bcb582d4d0de276f321f9d961383d264758
yonacr <yona.rasolonjatovo@staff.blueline.mg>
2021-08-28 13:09:53 +0300

    remove: failing test

--- faq/tests/views/question.py
+++ faq/tests/views/question.py
@@ -27,27 +27,4 @@ class TestQuestionListView(TestCase):
         self.assertIn('unanswered', response.context)
         self.assertIn(unanswered, response.context['unanswered'])
 
-
-class TestQuestionUpdateView(TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.client = Client()
-        cls.user = baker.make('User')
-
-    def test_post_new_answer(self):
-        question = baker.make('Question')
-        self.client.force_login(self.user)
-        form_data = {
-            'answer_text': "This is my answer",
-            'question_category': baker.make('Category')
-        }
-
-        response = self.client.post(
-            reverse('faq:question_update', kwargs={'id': question.id}),
-            form_data=form_data
-        )
-        raise ValueError("sorry I don't know how to test the two forms together")
-
 # EOF
